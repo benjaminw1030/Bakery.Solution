437a0037833c90343f74c4c79c46bf4be9bd920d
Ben Wilson <benjaminw1030@gmail.com>
2021-09-24 08:50:04 -0700

    pass cost method

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -9,9 +9,21 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      int breadOrder = 2;
-      Bread newBread = new Bread(breadOrder);
+      Bread newBread = new Bread(2);
       Assert.AreEqual(2, newBread.Count);
+    }
+
+    [TestMethod]
+    public void Cost_ReturnsPriceOfBreadOrder_Int()
+    {
+      Bread order1 = new Bread(1);
+      Bread order2 = new Bread(2);
+      Bread order3 = new Bread(3);
+      Bread order4 = new Bread(4);
+      Assert.AreEqual(5, order1.Cost());
+      Assert.AreEqual(10, order2.Cost());
+      Assert.AreEqual(10, order3.Cost());
+      Assert.AreEqual(15, order4.Cost());
     }
   }
 }
