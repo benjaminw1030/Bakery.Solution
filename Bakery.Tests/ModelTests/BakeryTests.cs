3775041035530d36daa91167e0547b727d13ad5d
Ben Wilson <benjaminw1030@gmail.com>
2021-09-24 08:57:11 -0700

    test getter/setter methods

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -7,10 +7,18 @@ namespace Bakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    public void BreadConstructor_ReturnsCount_Bread()
     {
       Bread newBread = new Bread(2);
       Assert.AreEqual(2, newBread.Count);
+    }
+
+    [TestMethod]
+    public void BreadSetter_AllowsChangeToCount_Int()
+    {
+      Bread newBread = new Bread(2);
+      newBread.Count += 1;
+      Assert.AreEqual(3, newBread.Count);
     }
 
     [TestMethod]
@@ -24,6 +32,25 @@ namespace Bakery.Tests
       Assert.AreEqual(10, order2.Cost());
       Assert.AreEqual(10, order3.Cost());
       Assert.AreEqual(15, order4.Cost());
+    }
+  }
+
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryGetter_ReturnsCount_Pastry()
+    {
+      Pastry newPastry = new Pastry(2);
+      Assert.AreEqual(2, newPastry.Count);
+    }
+
+    [TestMethod]
+    public void PastrySetter_AllowsChangeToCount_Int()
+    {
+      Pastry newPastry = new Pastry(2);
+      newPastry.Count -= 1;
+      Assert.AreEqual(1, newPastry.Count);
     }
   }
 }
